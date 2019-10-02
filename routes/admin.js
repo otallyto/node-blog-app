628b43cf56447a1a2813cb3c8773d5beb32be9e5
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2019-10-02 17:38:55 -0400

    Listando posts

--- routes/admin.js
+++ routes/admin.js
@@ -13,10 +13,17 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categorias', (req, res) => {
-    res.render("admin/categorias")
+    Categoria.find().sort({date: 1}).then((categorias)=>{
+        res.render("admin/categorias",{categorias: categorias})
+    }).catch((err)=>{
+        req.flash("error_msg", "Houve um erro ao listar as categorias");
+        res.redirect('/admin');
+    })
+   
 });
 
 router.get('/categorias/add', (req, res) => {
+    
     res.render("admin/addcategorias")
 });
 
