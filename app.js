0edeb219792c03671f84c4d5f9bb7df46e5ed1c2
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2019-10-02 16:51:25 -0400

    Adicionado sessões e tratamentos de erro

--- app.js
+++ app.js
@@ -6,8 +6,24 @@ const app = express();
 const admin = require('./routes/admin');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 //Configurações
+    //Sessão
+    app.use(session({
+        secret: 'cursodenode',
+        resave: true,
+        saveUninitialized: true
+    }));
+    //Flash
+    app.use(flash());
+    //Middleware
+    app.use((req,res,next)=>{
+        res.locals.success_msg = req.flash("success_msg");
+        res.locals.error_msg = req.flash("error_msg");
+        next();
+    });
     //Body Parser
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
@@ -22,12 +38,6 @@ const mongoose = require('mongoose');
     //Public
     app.use(express.static(path.join(__dirname,'public')));
 
-    //Middleware
-    app.use((req,res,next)=>{
-        console.log('Oi seu sou um midware');
-        next();
-    })
-
 //Rotas
 app.use('/admin',admin);
 
@@ -36,4 +46,4 @@ app.use('/admin',admin);
 const PORT = 8081;
 app.listen(PORT,()=>{
     console.log('servidor rodando http://localhost:8081');
-})
+});
